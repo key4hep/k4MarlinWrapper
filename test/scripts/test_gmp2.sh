b639697af07f76bb9cba969a8c49119f3d9e4e1b
Placido Fernandez Declara <placido.fernandez@cern.ch>
2020-05-13 18:24:35 +0200

    Generate input file automatically and run test

---
+++ test/scripts/test_gmp2.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ ! -d $GMP_tests_DIR/inputFiles/ ]; then
+  mkdir $GMP_tests_DIR/inputFiles/
+fi
+
+
+if [ ! -f $GMP_tests_DIR/inputFiles/testSimulation.slcio ]; then
+  echo "Input file not found. Generating one..."
+  ddsim \
+    --steeringFile $ILCSOFT/ClicPerformance/HEAD/clicConfig/clic_steer.py \
+    --inputFiles $ILCSOFT/ClicPerformance/HEAD/Tests/yyxyev_000.stdhep -N 4 \
+    --compactFile $ILCSOFT/lcgeo/HEAD/CLIC/compact/CLIC_o3_v14/CLIC_o3_v14.xml \
+    --outputFile $GMP_tests_DIR/inputFiles/testSimulation.slcio
+fi
+
+../run $GMP_tests_DIR/../runit/gmprun $GMP_tests_DIR/gaudi_opts/test_gmp2.py
