58e7a270a8065a8bd2eaf50d55fb9253eb3eff23
Placido Fernandez Declara <placido.fernandez@cern.ch>
2020-06-08 18:24:38 +0200

    fix Gaudi paths

--- .github/scripts/gmp_build_and_test.sh
+++ .github/scripts/gmp_build_and_test.sh
@@ -4,26 +4,13 @@ yum -y install make git
 
 source /cvmfs/clicdp.cern.ch/iLCSoft/lcg/97/nightly/x86_64-centos7-gcc8-opt/init_ilcsoft.sh
 
-# Get Gaudi and compile it
-git clone https://gitlab.cern.ch/gaudi/Gaudi.git
-cd Gaudi
-mkdir build install; cd build
-cmake \
-  -DCMAKE_INSTALL_PREFIX=../install \
-  -DBoost_NO_BOOST_CMAKE=TRUE \
-  ..
-make -j 2
-make install
-
-cd ../../
-
 # Compile GMP pointing to Gaudi
 cd /GMP
 mkdir build install; cd build
 cmake \
   -DCMAKE_INSTALL_PREFIX=../install \
-  -DGaudi_DIR="/Gaudi/install" \
-  -DGaudiProject_DIR="/Gaudi/install/cmake" \
+  -DGaudi_DIR="/shared_gaudi_volume" \
+  -DGaudiProject_DIR="/shared_gaudi_volume/cmake" \
   ..
 make -j 2
 make install
