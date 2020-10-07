244a706f633c18295cc273192b9c4d62dd33d5a8
Placido Fernandez Declara <placido.fernandez@cern.ch>
2020-10-07 09:38:18 +0200

    add verbose for test output

--- test/scripts/test_gmp1.sh
+++ test/scripts/test_gmp1.sh
@@ -6,8 +6,7 @@ if [ ! -d $GMP_tests_DIR/inputFiles/ ]; then
 fi
 
 if [ ! -f $GMP_tests_DIR/inputFiles/muons.slcio ]; then
-  wget https://github.com/AIDASoft/DD4hep/raw/master/DDTest/inputFiles/muons.slcio
-  mv muons.slcio $GMP_tests_DIR/inputFiles/muons.slcio
+  wget https://github.com/AIDASoft/DD4hep/raw/master/DDTest/inputFiles/muons.slcio -P $GMP_tests_DIR/inputFiles/
 fi
 
 ../run gaudirun.py $GMP_tests_DIR/gaudi_opts/test_gmp1.py
