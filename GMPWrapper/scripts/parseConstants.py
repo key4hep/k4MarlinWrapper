4f76a3339b0fbf578e9e1cb1f8201f38130ac342
Placido Fernandez Declara <placido.fernandez@cern.ch>
2020-10-13 10:01:38 +0200

    updated runtime conversion script

--- GMPWrapper/scripts/parseConstants.py
+++ GMPWrapper/scripts/parseConstants.py
@@ -1,17 +1,13 @@
 import re
 
-def parseConstants(CONSTANTS):
-  CONSTANTS_B = dict()
-  for key,value in CONSTANTS.items():
-    if value:
-      captured_patterns = re.findall('\$\{\w*\}', value)
-      if len(captured_patterns) > 0:
-        for pattern in captured_patterns:
-          CONSTANTS_B[key] = CONSTANTS[key].replace(pattern, CONSTANTS[pattern[2:-1]])
-      else:
-        CONSTANTS_B[key] = value
-
-  return CONSTANTS_B
+def parseConstants(constants):
+  again = True
+  while again:
+    again = False
+    for key,value in constants.items():
+      if (value):
+        constants[key] = value % constants
+        again = (again) or (constants[key] != value)
 
 if __name__ == "__main__":
   parseConstants()
