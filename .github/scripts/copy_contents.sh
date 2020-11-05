7da8ad5bb55b34de6086234b1782a5650bdbdeb0
Placido Fernandez Declara <placido.fernandez@cern.ch>
2020-11-05 22:09:01 +0100

    change compilation to HSF and remove Gaudi compilation

--- .github/scripts/copy_contents.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-### Run like this beacuse docker exec won't do bash expansion for the *
-cp -r /gaudi/* /shared_gaudi_volume/
--- cmake/gaudi_binary_tag.cmake
+++ cmake/gaudi_binary_tag.cmake
@@ -1,31 +1,7 @@
 # get_host_binary_tag.py is not executable in normal gaudi installations,
 # which silently fails in the BinaryTagUtils from Gaudi
 
-if (NOT HOST_BINARY_TAG)
-
-  find_program(HOST_BINARY_TAG_COMMAND
-               NAMES host-binary-tag get_host_binary_tag.py
-               PATHS "${Gaudi_DIR}/cmake")
-  execute_process(COMMAND python ${HOST_BINARY_TAG_COMMAND}
-     OUTPUT_VARIABLE HOST_BINARY_TAG
-     RESULT_VARIABLE HOST_TAG_RETURN
-     ERROR_VARIABLE  HOST_BINARY_ERROR
-     OUTPUT_STRIP_TRAILING_WHITESPACE)
-  IF(NOT HOST_BINARY_TAG_COMMAND OR HOST_TAG_RETURN OR NOT HOST_BINARY_TAG)
-    message(SEND_ERROR "Failed to execute ${HOST_BINARY_TAG_COMMAND}")
-    message(SEND_ERROR "HOST_BINARY_TAG: ${HOST_BINARY_TAG}")
-    message(SEND_ERROR "Return Code ${HOST_TAG_RETURN}")
-    message(SEND_ERROR "Error Message ${HOST_BINARY_ERROR}")
-  ENDIF()
-  set(HOST_BINARY_TAG ${HOST_BINARY_TAG} CACHE STRING "BINARY_TAG of the host")
-  set(BINARY_TAG ${HOST_BINARY_TAG})
-
-  message(STATUS "Using      binary tag ${BINARY_TAG}")
-  message(STATUS "Using host binary tag ${HOST_BINARY_TAG}")
-
-else()
-
-  set(BINARY_TAG ${HOST_BINARY_TAG})
-
+if( NOT DEFINED ENV{LCG_VERSION})
+  set(HOST_BINARY_TAG x86_64-linux-gcc9-opt)
 endif()
-
+find_package(GaudiProject)
