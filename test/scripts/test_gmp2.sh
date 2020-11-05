f30bb6ec9761c21ef201ef5092ec893ec5fc9840
Placido Fernandez Declara <placido.fernandez@cern.ch>
2020-11-06 00:12:10 +0100

    fix ddsim file generation with  path

--- test/scripts/test_gmp2.sh
+++ test/scripts/test_gmp2.sh
@@ -7,6 +7,9 @@ fi
 
 if [ ! -f $GMP_tests_DIR/inputFiles/testSimulation.slcio ]; then
   echo "Input file not found. Generating one..."
+  if [ -z ${ILCSOFT} ]; then
+    ILCSOFT="/cvmfs/clicdp.cern.ch/iLCSoft/lcg/97/nightly/x86_64-centos7-gcc8-opt"
+  fi
   ddsim \
     --steeringFile $ILCSOFT/ClicPerformance/HEAD/clicConfig/clic_steer.py \
     --inputFiles $ILCSOFT/ClicPerformance/HEAD/Tests/yyxyev_000.stdhep -N 4 \
