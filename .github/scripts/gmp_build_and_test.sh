59fc108af4fc4ba508ab2a476903caab0b3be90e
Placido Fernandez Declara <placido.fernandez@cern.ch>
2020-11-05 22:09:01 +0100

    change compilation to HSF and remove Gaudi compilation

--- .github/scripts/gmp_build_and_test.sh
+++ .github/scripts/gmp_build_and_test.sh
@@ -2,16 +2,13 @@
 
 yum -y install make git wget
 
-source /cvmfs/clicdp.cern.ch/iLCSoft/lcg/97/nightly/x86_64-centos7-gcc8-opt/init_ilcsoft.sh
+source /cvmfs/sw.hsf.org/key4hep/setup.sh
 
 # Compile GMP pointing to Gaudi
 cd /GMP
 mkdir build install; cd build
 cmake \
   -DCMAKE_INSTALL_PREFIX=../install \
-  -DGaudi_DIR="/shared_gaudi_volume" \
-  -DGaudiProject_DIR="/shared_gaudi_volume/cmake" \
-  -DHOST_BINARY_TAG=skylake-centos7-gcc8-opt \
   .. && \
 make -j 2 && \
 make install && \
