e25ec6461a228c058bf0bb8bd89e71e5a9c97bc4
Placido Fernandez Declara <placido.fernandez@cern.ch>
2021-01-15 15:24:26 +0100

    remove local paths, add comments and remove unnecessary file

--- cmake/gaudi_binary_tag.cmake
+++
@@ -1,7 +0,0 @@
-# get_host_binary_tag.py is not executable in normal gaudi installations,
-# which silently fails in the BinaryTagUtils from Gaudi
-
-if( NOT DEFINED ENV{LCG_VERSION})
-  set(HOST_BINARY_TAG x86_64-linux-gcc9-opt)
-endif()
-find_package(Gaudi)
