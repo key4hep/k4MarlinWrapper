370d5e7d4267f42e4a746acfded9bd3c9d0e4df4
Placido Fernandez Declara <placido.fernandez@cern.ch>
2021-01-26 01:44:57 +0100

    DataHandle working with loop over tracks and recoparticles

--- k4MarlinWrapper/k4MarlinWrapper/converters/LCIO2EDM4hep.h
+++ k4MarlinWrapper/k4MarlinWrapper/converters/LCIO2EDM4hep.h
@@ -2,6 +2,7 @@
 
 // GAUDI
 #include "GaudiAlg/GaudiAlgorithm.h"
+#include "Gaudi/Property.h"
 
 // FWCore
 #include "k4FWCore/DataHandle.h"
@@ -10,9 +11,11 @@
 #include "edm4hep/MCParticle.h"
 #include "edm4hep/MCParticleData.h"
 #include "edm4hep/MCParticleCollection.h"
-// #include "edm4hep/ReconstructedParticle.h"
+#include "edm4hep/ReconstructedParticle.h"
 #include "edm4hep/ReconstructedParticleCollection.h"
 #include "edm4hep/ReconstructedParticleData.h"
+#include "edm4hep/RecoParticleRefData.h"
+#include "edm4hep/TrackCollection.h"
 
 class LCIO2EDM4hep : public GaudiAlgorithm {
 public:
@@ -23,6 +26,9 @@ public:
   virtual StatusCode finalize();
 
 private:
+
+  int m_member = 0;
+  // DataHandle<edm4hep::MCParticleCollection> mcps_handle {"Particle", Gaudi::DataHandle::Reader, this};
   /// Handle for the edm4hep MC particles to be read
   // DataHandle<edm4hep::ReconstructedParticleData> m_e4hhandle {"ReconstructedParticles", Gaudi::DataHandle::Reader, this};
   // DataHandle<edm4hep::MCParticleData> m_e4hhandle {"Particle", Gaudi::DataHandle::Reader, this};
--- k4MarlinWrapper/src/components/LCIO2EDM4hep.cpp
+++ k4MarlinWrapper/src/components/LCIO2EDM4hep.cpp
@@ -9,7 +9,8 @@
 #include <ctime>
 #include <chrono>
 
-#include "edm4hep/RecoParticleRefData.h"
+#include <LCEventWrapper.h>
+#include <EVENT/LCEvent.h>
 
 #include "lcio.h"
 #include "IO/LCWriter.h"
@@ -19,136 +20,158 @@
 #include "IMPL/MCParticleImpl.h"
 #include "IMPL/ReconstructedParticleImpl.h"
 #include "IMPL/TrackImpl.h"
+#include "IMPL/TrackStateImpl.h"
 #include "IMPL/ParticleIDImpl.h"
 
 DECLARE_COMPONENT(LCIO2EDM4hep)
 
 LCIO2EDM4hep::LCIO2EDM4hep(const std::string& name, ISvcLocator* svcLoc) : GaudiAlgorithm(name, svcLoc) {
-  // declareProperty("ReconstructedParticles", m_e4hhandle, "Generated particles collection (input)");
+  // declareProperty("Particle", mcps_handle, "");
 }
 
 StatusCode LCIO2EDM4hep::initialize() {
   info() << "start LCIO2EDM4hep::initialize()\n";
 
-  TFile* input_file = new TFile("/eos/experiment/fcc/ee/generation/DelphesEvents/fcc_tmp/p8_ee_Ztautau_ecm91_EvtGen_Tau2MuGamma/events_001720714.root","READ");
-  TTree* input_tree = (TTree*)input_file->Get("events");
-
-  int nentries = int(input_tree->GetEntries());
-
-  info() << "nentries: " << nentries << "\n";
-
-  // LCIO stuff to write file
-  const std::string output_filename {"output.slcio"};
-  IO::LCWriter* my_lcwriter = nullptr;
-  const std::string detector_name {"ToyTracker"};
-
-  if (!output_filename.empty()) {
-    my_lcwriter = lcio::LCFactory::getInstance()->createLCWriter();
-    my_lcwriter->open(output_filename , lcio::LCIO::WRITE_NEW);
-  }
-
-  // loop over the events
-  for (int e = 0; e < nentries; ++e) {
-
-    // process just first 3
-    if (e == 3) break;
-
-    std::vector<edm4hep::ReconstructedParticleData> rp_vec;
-    std::vector<edm4hep::ReconstructedParticleData>* prp_vec = &rp_vec;
-
-    TBranch* rp_branch = input_tree->GetBranch("ReconstructedParticles");
-    rp_branch->SetAddress(&prp_vec);
-    rp_branch->GetEntry(e); // here is the loop over the events
-
-    std::vector<edm4hep::TrackState> eft_vec;
-    std::vector<edm4hep::TrackState>* peft_vec = &eft_vec;
-
-    TBranch* eft_branch = input_tree->GetBranch("EFlowTrack_1");
-    eft_branch->SetAddress(&peft_vec);
-    eft_branch->GetEntry(e);
-
-    info() << "number of rec particles: " << rp_vec.size() << "\n";
-
-    // This would be done for every event
-    auto event = new lcio::LCEventImpl;
-    event->setEventNumber(e);
-    event->setDetectorName(detector_name);
-    event->setRunNumber(1);
-    const auto p1 = (std::chrono::system_clock::now().time_since_epoch()).count();
-    event->setTimeStamp(p1);
-
-    auto* recops = new lcio::LCCollectionVec(lcio::LCIO::RECONSTRUCTEDPARTICLE);
-    auto* tracks = new lcio::LCCollectionVec(lcio::LCIO::TRACK);
-
-    // loop over EDM4hep particles
-    // for (auto& rp : rp_vec) {
-    for(uint rp=0; rp < rp_vec.size(); ++rp) {
-
-      // print EDM4hep particle for checks
-      info() << "Particle Info:\n";
-      info() << "charge: " << rp_vec.at(rp).charge << "\n";
-      info() << "momentum.x: " << rp_vec.at(rp).momentum.x << "\n";
-      info() << "momentum.y: " << rp_vec.at(rp).momentum.y << "\n";
-      info() << "momentum.z: " << rp_vec.at(rp).momentum.z << "\n";
-      info() << "energy: " << rp_vec.at(rp).energy << "\n";
-      info() << "mass: " << rp_vec.at(rp).mass << "\n";
-      info() << "tracks_begin: " << rp_vec.at(rp).tracks_begin << "\n";
-      info() << "\n";
-
-      auto* recp = new lcio::ReconstructedParticleImpl;
-      recp->setCharge(rp_vec.at(rp).charge);
-      double m[3] = {rp_vec.at(rp).momentum.x, rp_vec.at(rp).momentum.y, rp_vec.at(rp).momentum.z};
-      recp->setMomentum(m);
-      recp->setEnergy(rp_vec.at(rp).energy);
-
-      if (rp_vec.at(rp).tracks_begin < eft_vec.size()) {
-        auto* track = new lcio::TrackImpl;
-        auto trkind = rp_vec.at(rp).tracks_begin;
-
-        track->setD0(eft_vec.at(trkind).D0);
-        track->setPhi(eft_vec.at(trkind).phi);
-        track->setOmega(eft_vec.at(trkind).omega);
-        track->setZ0(eft_vec.at(trkind).Z0);
-        // track->setTanLambda(eft_vec.at(trkind).tanLamda);
-        track->subdetectorHitNumbers().resize(50);
-
-        std::array<float, 15> cov;
-        // float *cov;
-        cov[0]  = rp_vec.at(rp).covMatrix[0];
-        cov[2]  = rp_vec.at(rp).covMatrix[5];
-        cov[5]  = rp_vec.at(rp).covMatrix[9];
-        cov[9]  = rp_vec.at(rp).covMatrix[12];
-        cov[14] = rp_vec.at(rp).covMatrix[14];
-
-        track->setCovMatrix(cov.data());
-
-        tracks->addElement(track);
-
-        recp->addTrack(track);
-        recops->addElement(recp);
-      }
-    }
-
-    event->addCollection(tracks, lcio::LCIO::TRACK);
-    event->addCollection(recops, lcio::LCIO::RECONSTRUCTEDPARTICLE);
-
-    my_lcwriter->writeEvent(event);
-  }
-
-  // clean up ?
-  my_lcwriter->flush();
-  my_lcwriter->close();
-  delete input_file;
-  // delete recops;
-  // delete tracks;
-  // delete event;
-
   return GaudiAlgorithm::initialize();
-  // return StatusCode::SUCCESS;
 }
 
 StatusCode LCIO2EDM4hep::execute() {
   info() << "LCIO2EDM4hep::execute()\n";
+
+  // TODO for TrackCollection
+  // Tracks handle
+  DataHandle<edm4hep::TrackCollection> tracks_handle {
+    "EFlowTrack", Gaudi::DataHandle::Reader, this};
+  const auto tracks_coll = tracks_handle.get();
+
+  std::cout << "TrackCollection size: " << tracks_coll->size() << "\n\n";
+
+  // ReconstructedParticles handle
+  DataHandle<edm4hep::ReconstructedParticleCollection> recos_handle {
+    "ReconstructedParticles", Gaudi::DataHandle::Reader, this};
+  const auto recos_coll = recos_handle.get();
+
+  std::cout << "ReconstructedParticles size: " << recos_coll->size() << "\n\n";
+
+  std::vector<lcio::TrackImpl*> lcio_tracks_vec;
+
+  // Loop over EDM4hep tracks converting them to lcio tracks
+  // including the track states
+  for (const auto edm_tr = tracks_coll->begin(); edm_tr != tracks_coll->end(); ++edm_tr) {
+
+    auto* lcio_tr = new lcio::TrackImpl;
+
+    lcio_tr->setTypeBit( edm_tr->getType() );
+    lcio_tr->setChi2( edm_tr->getChi2() );
+    lcio_tr->setNdf( edm_tr->getNdf() );
+    lcio_tr->setdEdx( edm_tr->getDEdx() );
+    lcio_tr->setdEdxError( edm_tr->getDEdxError() );
+    lcio_tr->setRadiusOfInnermostHit( edm_tr->getRadiusOfInnermostHit() );
+
+    // Loop over the track states in the track
+    podio::RelationRange<edm4hep::TrackState> edm_track_states = edm_tr->getTrackStates();
+    for (auto& tr_state : edm_track_states) {
+
+      std::array<float, 15> cov = {}; // zero-init
+      cov[0]  = tr_state.covMatrix[0];
+      cov[2]  = tr_state.covMatrix[5];
+      cov[5]  = tr_state.covMatrix[9];
+      cov[9]  = tr_state.covMatrix[12];
+      cov[14] = tr_state.covMatrix[14];
+      std::array<float, 15> refP = {
+        tr_state.referencePoint.x, tr_state.referencePoint.y, tr_state.referencePoint.z};
+
+      auto* lcio_tr_state = new lcio::TrackStateImpl(
+        tr_state.location,
+        tr_state.D0,
+        tr_state.phi ,
+        tr_state.omega,
+        tr_state.Z0,
+        tr_state.tanLambda,
+        cov.data(),
+        refP.data()
+      );
+
+      lcio_tr->addTrackState( lcio_tr_state ) ;
+
+      // Default TrackState?
+      // lcio_tr_state->setLocation( tr_state.location );
+      // lcio_tr->setD0( tr_state.D0 );
+      // lcio_tr->setPhi( tr_state.phi );
+      // lcio_tr->setOmega( tr_state.omega );
+      // lcio_tr->setZ0( tr_state.Z0 );
+      // lcio_tr->setTanLambda( tr_state.tanLambda );
+      // lcio_tr->setCovMatrix( cov.data() );
+      // lcio_tr->setReferencePoint( refP.data() );
+    }
+
+    lcio_tracks_vec.push_back(lcio_tr);
+
+  }
+
+
+  // LCIO event to populate
+  const std::string detector_name {"ToyTracker"};
+  auto lcio_event = new lcio::LCEventImpl;
+  // lcio_event->setEventNumber(0);
+  lcio_event->setDetectorName(detector_name);
+  lcio_event->setRunNumber(1);
+  const auto p1 = (std::chrono::system_clock::now().time_since_epoch()).count();
+  lcio_event->setTimeStamp(p1);
+
+  // // Register LCIO event in TES
+  // auto pO = std::make_unique<LCEventWrapper>(lcio_event);
+  // const StatusCode sc = eventSvc()->registerObject("/Event/LCEvent", pO.release());
+  // if (sc.isFailure()) {
+  //   error() << "Failed to store the LCEvent" << endmsg;
+  //   return sc;
+  // }
+
+  // // Populate LCIO event with EDM4hep info
+  // auto* recops = new lcio::LCCollectionVec(lcio::LCIO::RECONSTRUCTEDPARTICLE);
+  // auto* tracks = new lcio::LCCollectionVec(lcio::LCIO::TRACK);
+
+
+  // TODO
+  // Loop over reconstructed particles
+  for (size_t i=0; i < recos_coll->size(); ++i) {
+    const auto p = recos_coll->at(i);
+
+    auto* lcio_recp = new lcio::ReconstructedParticleImpl;
+
+    lcio_recp->setType(p.getType());
+    float m[3] = {p.getMomentum()[0], p.getMomentum()[1], p.getMomentum()[2]};
+    lcio_recp->setMomentum(m);
+    lcio_recp->setEnergy(p.getEnergy());
+
+    lcio_recp->setCovMatrix(p.getCovMatrix().data());
+
+    lcio_recp->setMass(p.getMass());
+    lcio_recp->setCharge(p.getCharge());
+    float rp[3] = {p.getReferencePoint()[0], p.getReferencePoint()[1], p.getReferencePoint()[2]};
+    lcio_recp->setReferencePoint(rp);
+
+    // ::edm4hep::ConstParticleID getParticleIDUsed()
+    // lcio_recp->setParticleIDUsed(EVENT::ParticleID*  pid);
+
+    lcio_recp->setGoodnessOfPID(p.getGoodnessOfPID());
+
+    // ::edm4hep::ConstVertex getStartVertex()
+    // lcio_recp->setStartVertex(EVENT::Vertex * sv);
+
+    // Add lcio converted tracks
+    for (auto& lcio_tr : lcio_tracks_vec) {
+
+      // addTrack( EVENT::Track* track)
+
+    }
+
+  }
+
+  // StatusCode sc = eventSvc()->registerObject("/Event/LCEvent", event);
+  // put(event, "/Event/SomeData");
+  // lcio_handle.put(event);
+
   return StatusCode::SUCCESS;
 }
 
