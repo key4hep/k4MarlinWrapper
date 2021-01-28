29bcff868b1ad74863d51b2e99c9eb25e9fee736
Placido Fernandez Declara <placido.fernandez@cern.ch>
2021-01-28 14:53:38 +0100

    wip added reconstructedparticle conversion

--- k4MarlinWrapper/k4MarlinWrapper/converters/LCIO2EDM4hep.h
+++ k4MarlinWrapper/k4MarlinWrapper/converters/LCIO2EDM4hep.h
@@ -16,14 +16,36 @@
 #include "edm4hep/ReconstructedParticleData.h"
 #include "edm4hep/RecoParticleRefData.h"
 #include "edm4hep/TrackCollection.h"
+#include "edm4hep/VertexConst.h"
+
+// LCIO
+#include <LCEventWrapper.h>
+#include <EVENT/LCEvent.h>
+
+#include "lcio.h"
+#include "IO/LCWriter.h"
+#include "IMPL/LCEventImpl.h"
+#include "IMPL/LCRunHeaderImpl.h"
+#include "IMPL/LCCollectionVec.h"
+#include "IMPL/MCParticleImpl.h"
+#include "IMPL/ReconstructedParticleImpl.h"
+#include "IMPL/TrackImpl.h"
+#include "IMPL/TrackStateImpl.h"
+#include "IMPL/VertexImpl.h"
+#include "IMPL/ParticleIDImpl.h"
 
 class LCIO2EDM4hep : public GaudiAlgorithm {
 public:
   LCIO2EDM4hep(const std::string& name, ISvcLocator* svcLoc);
 
+  void addLCIOConvertedTracks(std::vector<lcio::TrackImpl*>& lcio_tracks_vec);
+  void addLCIOReconstructedParticles(std::vector<lcio::ReconstructedParticleImpl*>& lcio_rec_particles_vec);
+
   virtual StatusCode initialize();
   virtual StatusCode execute();
   virtual StatusCode finalize();
+
+
 
 private:
 
