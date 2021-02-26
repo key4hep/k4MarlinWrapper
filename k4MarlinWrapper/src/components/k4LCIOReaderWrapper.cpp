5aae929a4cb18d30743c3db7369343080bc7ac46
Placido Fernandez Declara <placido.fernandez@cern.ch>
2021-02-26 10:15:25 +0100

    templated function for conversion

--- k4MarlinWrapper/src/components/k4LCIOReaderWrapper.cpp
+++ k4MarlinWrapper/src/components/k4LCIOReaderWrapper.cpp
@@ -19,6 +19,28 @@ StatusCode k4LCIOReaderWrapper::finalize() {
   return GaudiTool::finalize();
 }
 
+template <typename T>
+void k4LCIOReaderWrapper::convertAndRegister(
+  const std::string& register_name,
+  const std::string& collection_name,
+  k4LCIOConverter* lcio_converter,
+  podio::CollectionIDTable* id_table) const
+{
+  // Pass name of collection to get
+  T* mycoll = dynamic_cast<T*>(lcio_converter->getCollection(collection_name));
+
+  if (mycoll == nullptr) {
+    // debug() << m_lcio2edm_params[i] << " collection was not found." << endmsg;
+    mycoll = new T();
+    mycoll->setID(id_table->add(collection_name));
+  }
+
+  // Register collection
+  EDMCollectionWrapper<T> mycoll_wrapper {mycoll};
+  auto pO = std::make_unique<EDMCollectionWrapper<T>>(mycoll_wrapper);
+  StatusCode reg_sc = evtSvc()->registerObject(register_name, pO.release());
+}
+
 
 StatusCode k4LCIOReaderWrapper::convertCollections(
   lcio::LCEventImpl* the_event)
@@ -30,9 +52,9 @@ StatusCode k4LCIOReaderWrapper::convertCollections(
   }
 
   // Set the event to the converter
-  podio::CollectionIDTable* m_table = new podio::CollectionIDTable();
-  k4LCIOConverter* m_converter = new k4LCIOConverter(m_table);
-  m_converter->set(the_event);
+  podio::CollectionIDTable* id_table = new podio::CollectionIDTable();
+  k4LCIOConverter* lcio_converter = new k4LCIOConverter(id_table);
+  lcio_converter->set(the_event);
 
   // Create eventHeader to fill
   auto* dest = new edm4hep::EventHeaderCollection();
@@ -45,66 +67,16 @@ StatusCode k4LCIOReaderWrapper::convertCollections(
   // Convert based on parameters
   for (int i = 0; i < m_lcio2edm_params.size(); i=i+3) {
 
-      // convertAdd(
-      //   m_lcio2edm_params[i],
-      //   m_lcio2edm_params[i+1],
-      //   m_lcio2edm_params[i+2],
-      //   the_event);
-
-
     // Is the type needed?
     if (m_lcio2edm_params[i] == "EVENT::ReconstructedParticle") {
-      // Pass name of collection to get
-      auto mycoll = dynamic_cast<edm4hep::ReconstructedParticleCollection*>(m_converter->getCollection(m_lcio2edm_params[i+1]));
-
-      if (mycoll == nullptr) {
-        debug() << m_lcio2edm_params[i] << " collection was not found." << endmsg;
-        mycoll = new edm4hep::ReconstructedParticleCollection();
-        mycoll->setID(m_table->add(m_lcio2edm_params[i+1]));
-      }
-
-      std::cout << "edm4hep::ReconstructedParticleCollection\n";
-      std::cout << *mycoll << std::endl;
-
-      // Register collection
-      EDMCollectionWrapper<edm4hep::ReconstructedParticleCollection> mycoll_wrapper {mycoll};
-      auto pO = std::make_unique<EDMCollectionWrapper<edm4hep::ReconstructedParticleCollection>>(mycoll_wrapper);
-      StatusCode reg_sc = evtSvc()->registerObject("/edm4hep/ReconstructedParticleCollection", pO.release());
-
+      convertAndRegister<edm4hep::ReconstructedParticleCollection>(
+        "/edm4hep/ReconstructedParticleCollection", m_lcio2edm_params[i+1], lcio_converter, id_table);
     } else if (m_lcio2edm_params[i] == "EVENT::ParticleID") {
-
-      auto* mycoll = dynamic_cast<edm4hep::ParticleIDCollection*>(m_converter->getCollection(m_lcio2edm_params[i+1]));
-
-      if (mycoll == nullptr) {
-        debug() << m_lcio2edm_params[i] << " collection was not found." << endmsg;
-        mycoll = new edm4hep::ParticleIDCollection();
-        mycoll->setID(m_table->add(m_lcio2edm_params[i+1]));
-      }
-
-      std::cout << "edm4hep::ParticleIDCollection\n";
-      std::cout << *mycoll << std::endl;
-
-      EDMCollectionWrapper<edm4hep::ParticleIDCollection> mycoll_wrapper {mycoll};
-      auto pO = std::make_unique<EDMCollectionWrapper<edm4hep::ParticleIDCollection>>(mycoll_wrapper);
-      StatusCode reg_sc = evtSvc()->registerObject("/edm4hep/ParticleIDCollection", pO.release());
-
+      convertAndRegister<edm4hep::ParticleIDCollection>(
+        "/edm4hep/ParticleIDCollection", m_lcio2edm_params[i+1], lcio_converter, id_table);
     } else if (m_lcio2edm_params[i] == "EVENT::Track") {
-
-      auto* mycoll = dynamic_cast<edm4hep::TrackCollection*>(m_converter->getCollection(m_lcio2edm_params[i+1]));
-
-      if (mycoll == nullptr) {
-        debug() << m_lcio2edm_params[i] << " collection was not found." << endmsg;
-        mycoll = new edm4hep::TrackCollection();
-        mycoll->setID(m_table->add(m_lcio2edm_params[i+1]));
-      }
-
-      std::cout << "edm4hep::TrackCollection\n";
-      std::cout << *mycoll << std::endl;
-
-      EDMCollectionWrapper<edm4hep::TrackCollection> mycoll_wrapper {mycoll};
-      auto pO = std::make_unique<EDMCollectionWrapper<edm4hep::TrackCollection>>(mycoll_wrapper);
-      StatusCode reg_sc = evtSvc()->registerObject("/edm4hep/TrackCollection", pO.release());
-
+      convertAndRegister<edm4hep::TrackCollection>(
+        "/edm4hep/TrackCollection", m_lcio2edm_params[i+1], lcio_converter, id_table);
     } else {
       error() << m_lcio2edm_params[i] << " conversion type not supported." << endmsg;
     }
