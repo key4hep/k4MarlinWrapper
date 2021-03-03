5bb4e4f29709586d040635e94dc5956629ee2059
Placido Fernandez Declara <placido.fernandez@cern.ch>
2021-03-03 19:02:02 +0100

    switch to nightlies to compile with last k4LCIOReader

--- .github/scripts/k4MarlinWrapper_build_and_test.sh
+++ .github/scripts/k4MarlinWrapper_build_and_test.sh
@@ -2,7 +2,8 @@
 
 yum -y install make git wget
 
-source /cvmfs/sw.hsf.org/key4hep/setup.sh
+# source /cvmfs/sw.hsf.org/key4hep/setup.sh
+source /cvmfs/sw-nightlies.hsf.org/key4hep/setup.sh
 
 # Compile k4MarlinWrapper
 cd /k4MarlinWrapper
