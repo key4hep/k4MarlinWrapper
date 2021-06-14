1356355f804654a0ccc96ec4c63f44e1c92f2397
Placido Fernandez Declara <placido.fernandez@cern.ch>
2021-06-14 11:01:55 +0200

    add optional external Catch2 and fix initial test

--- test/src/unit_tests.cpp
+++ test/src/unit_tests.cpp
@@ -1,7 +1,5 @@
 #include <catch2/catch_all.hpp>
 
-#include "k4MarlinWrapper/converters/EDM4hep2Lcio.h"
-
 unsigned int Factorial( unsigned int number ) {
     return number <= 1 ? number : Factorial(number-1)*number;
 }
@@ -11,10 +9,4 @@ TEST_CASE( "Factorials are computed", "[factorial]" ) {
     REQUIRE( Factorial(2) == 2 );
     REQUIRE( Factorial(3) == 6 );
     REQUIRE( Factorial(10) == 3628800 );
-}
-
-TEST_CASE( "Parameter sorting", "[parameters]") {
-    std::vector<std::string> fake_data1 = {"SimTrackerHit", "hits_VXD_barrel", "hits_VXD_barrel_LCIO"};
-
-    REQUIRE(  )
 }
