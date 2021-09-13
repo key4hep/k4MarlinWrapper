9b24da646b527b8ab720670663b73f54370679fb
Placido Fernandez Declara <placido.fernandez@cern.ch>
2021-09-13 16:55:57 +0200

    add sequence stop on not finding more events to process

---
+++ test/gaudi_opts/over_total_events.py
@@ -0,0 +1,68 @@
+import os
+
+from Gaudi.Configuration import *
+
+from Configurables import LcioEvent, EventDataSvc, MarlinProcessorWrapper
+algList = []
+evtsvc = EventDataSvc()
+
+read = LcioEvent()
+read.OutputLevel = DEBUG
+read.Files = ["$k4MarlinWrapper_tests_DIR/inputFiles/muons.slcio"]
+algList.append(read)
+
+# assign parameters by hand, in future parse Marlin.xml file in python
+# and convert to list of processors and parameters
+procA = MarlinProcessorWrapper("AidaProcessor")
+procA.OutputLevel = DEBUG
+procA.ProcessorType = "AIDAProcessor"
+procA.Parameters = {"FileName": ["histograms"],
+                    "FileType": ["root"],
+                    "Compress": ["1"],
+                    "Verbosity": ["DEBUG"],
+                    }
+algList.append(procA)
+
+
+proc0 = MarlinProcessorWrapper("EventNumber")
+proc0.OutputLevel = DEBUG
+proc0.ProcessorType = "Statusmonitor"
+proc0.Parameters = {"HowOften": ["1"],
+                    "Verbosity": ["DEBUG"],
+                    }
+algList.append(proc0)
+
+
+proc1 = MarlinProcessorWrapper("InitDD4hep")
+proc1.OutputLevel = DEBUG
+proc1.ProcessorType = "InitializeDD4hep"
+proc1.Parameters = {#"EncodingStringParameter": ["GlobalTrackerReadoutID"],
+                    "DD4hepXMLFile": [os.path.join(os.environ.get('LCGEO'), 'CLIC/compact/CLIC_o2_v04/CLIC_o2_v04.xml')]
+                    }
+algList.append(proc1)
+
+
+digiVxd = MarlinProcessorWrapper("VXDBarrelDigitiser")
+digiVxd.OutputLevel = DEBUG
+digiVxd.ProcessorType = "DDPlanarDigiProcessor"
+digiVxd.Parameters = {
+    "SubDetectorName": ["Vertex"],
+    "IsStrip": ["false"],
+    "ResolutionU": ["0.003", "0.003", "0.003", "0.003", "0.003", "0.003"],
+    "ResolutionV": ["0.003", "0.003", "0.003", "0.003", "0.003", "0.003"],
+    "SimTrackHitCollectionName": ["VertexBarrelCollection"],
+    "SimTrkHitRelCollection": ["VXDTrackerHitRelations"],
+    "TrackerHitCollectionName": ["VXDTrackerHits"],
+    "Verbosity": ["DEBUG"],
+                    }
+
+algList.append(digiVxd)
+
+
+from Configurables import ApplicationMgr
+ApplicationMgr( TopAlg = algList,
+                EvtSel = 'NONE',
+                EvtMax   = 15,
+                ExtSvc = [evtsvc],
+                OutputLevel=DEBUG
+)
