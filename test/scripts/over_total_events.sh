2f2084504863f0f0b2cdbbc10fa0fe43fe453a57
Placido Fernandez Declara <placido.fernandez@cern.ch>
2021-09-15 11:39:09 +0200

    use k4run to run tests; remove redundant options files

--- test/scripts/over_total_events.sh
+++ test/scripts/over_total_events.sh
@@ -9,4 +9,4 @@ if [ ! -f $k4MarlinWrapper_tests_DIR/inputFiles/muons.slcio ]; then
   wget https://github.com/AIDASoft/DD4hep/raw/master/DDTest/inputFiles/muons.slcio -P $k4MarlinWrapper_tests_DIR/inputFiles/
 fi
 
-../run gaudirun.py $k4MarlinWrapper_tests_DIR/gaudi_opts/over_total_events.py
+../run k4run $k4MarlinWrapper_tests_DIR/gaudi_opts/test_k4MarlinWrapper1.py --num-events=15
