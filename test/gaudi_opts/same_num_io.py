0498206956d183e0609eb67dfdb41316f0ea2f65
Placido Fernandez Declara <placido.fernandez@cern.ch>
2021-09-15 17:17:18 +0200

    add test to check same num events for input and output

---
+++ test/gaudi_opts/same_num_io.py
@@ -0,0 +1,34 @@
+import os
+
+from Gaudi.Configuration import *
+
+from Configurables import LcioEvent, EventDataSvc, MarlinProcessorWrapper
+algList = []
+evtsvc = EventDataSvc()
+
+read = LcioEvent()
+read.OutputLevel = DEBUG
+read.Files = ["$k4MarlinWrapper_tests_DIR/inputFiles/muons.slcio"]
+algList.append(read)
+
+Output_DST = MarlinProcessorWrapper("Output_DST")
+Output_DST.OutputLevel = WARNING 
+Output_DST.ProcessorType = "LCIOOutputProcessor" 
+Output_DST.Parameters = {
+                         "DropCollectionNames": [],
+                         "DropCollectionTypes": ["MCParticle", "LCRelation", "SimCalorimeterHit", "CalorimeterHit", "SimTrackerHit", "TrackerHit", "TrackerHitPlane", "Track", "ReconstructedParticle", "LCFloatVec", "Clusters"],
+                         "FullSubsetCollections": ["EfficientMCParticles", "InefficientMCParticles", "MCPhysicsParticles"],
+                         "KeepCollectionNames": ["MCParticlesSkimmed", "MCPhysicsParticles", "RecoMCTruthLink", "SiTracks", "SiTracks_Refitted", "PandoraClusters", "PandoraPFOs", "SelectedPandoraPFOs", "LooseSelectedPandoraPFOs", "TightSelectedPandoraPFOs", "LE_SelectedPandoraPFOs", "LE_LooseSelectedPandoraPFOs", "LE_TightSelectedPandoraPFOs", "LumiCalClusters", "LumiCalRecoParticles", "BeamCalClusters", "BeamCalRecoParticles", "MergedRecoParticles", "MergedClusters", "RefinedVertexJets", "RefinedVertexJets_rel", "RefinedVertexJets_vtx", "RefinedVertexJets_vtx_RP", "BuildUpVertices", "BuildUpVertices_res", "BuildUpVertices_RP", "BuildUpVertices_res_RP", "BuildUpVertices_V0", "BuildUpVertices_V0_res", "BuildUpVertices_V0_RP", "BuildUpVertices_V0_res_RP", "PrimaryVertices", "PrimaryVertices_res", "PrimaryVertices_RP", "PrimaryVertices_res_RP", "RefinedVertices", "RefinedVertices_RP"],
+                         "LCIOOutputFile": ["Output_DST.slcio"],
+                         "LCIOWriteMode": ["WRITE_NEW"]
+                         }
+
+algList.append(Output_DST)
+
+from Configurables import ApplicationMgr
+ApplicationMgr( TopAlg = algList,
+                EvtSel = 'NONE',
+                EvtMax   = 10,
+                ExtSvc = [evtsvc],
+                OutputLevel=DEBUG
+)
---
+++ test/scripts/same_num_io.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# set -eu
+
+if [ ! -d $k4MarlinWrapper_tests_DIR/inputFiles/ ]; then
+  mkdir $k4MarlinWrapper_tests_DIR/inputFiles
+fi
+
+if [ ! -f $k4MarlinWrapper_tests_DIR/inputFiles/muons.slcio ]; then
+  wget https://github.com/AIDASoft/DD4hep/raw/master/DDTest/inputFiles/muons.slcio -P $k4MarlinWrapper_tests_DIR/inputFiles/
+fi
+
+../run k4run $k4MarlinWrapper_tests_DIR/gaudi_opts/same_num_io.py
+
+input_num_events=$(lcio_event_counter $k4MarlinWrapper_tests_DIR/inputFiles/muons.slcio)
+output_num_events=$(lcio_event_counter Output_DST.slcio)
+
+if [ "$input_num_events" == "$output_num_events" ] || [ "$input_num_events" == 10 ]; then
+  echo "Input and output have same number of events"
+else
+  echo "ERROR: different number of events for input and output"
+fi
