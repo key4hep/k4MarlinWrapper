c31f5e763c38c07425d7db627f68a59a75f71a94
Placido Fernandez Declara <placido.fernandez@cern.ch>
2021-09-22 17:06:22 +0200

    test correct number of output events when indicating to read all

--- test/scripts/same_num_io.sh
+++ test/scripts/same_num_io.sh
@@ -9,7 +9,7 @@ if [ ! -f $k4MarlinWrapper_tests_DIR/inputFiles/muons.slcio ]; then
   wget https://github.com/AIDASoft/DD4hep/raw/master/DDTest/inputFiles/muons.slcio -P $k4MarlinWrapper_tests_DIR/inputFiles/
 fi
 
-../run k4run $k4MarlinWrapper_tests_DIR/gaudi_opts/same_num_io.py
+../run k4run $k4MarlinWrapper_tests_DIR/gaudi_opts/same_num_io.py --num-events=-1
 
 input_num_events=$(lcio_event_counter $k4MarlinWrapper_tests_DIR/inputFiles/muons.slcio)
 output_num_events=$(lcio_event_counter Output_DST.slcio)
