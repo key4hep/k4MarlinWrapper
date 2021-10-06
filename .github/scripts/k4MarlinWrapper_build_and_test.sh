da8823ae2574c280f2f97b0551bcfed06816fabc
Placido Fernandez Declara <placido.fernandez@cern.ch>
2021-10-06 13:47:23 +0200

    update github actions to run regular and nightlies testing

--- .github/scripts/k4MarlinWrapper_build_and_test.sh
+++ .github/scripts/k4MarlinWrapper_build_and_test.sh
@@ -2,9 +2,6 @@
 
 yum -y install make git wget
 
-#source /cvmfs/sw.hsf.org/key4hep/setup.sh
-source /cvmfs/sw-nightlies.hsf.org/key4hep/setup.sh
-
 # Compile k4MarlinWrapper
 cd /k4MarlinWrapper
 mkdir build install; cd build
