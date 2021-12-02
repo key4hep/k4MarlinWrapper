a92115e367bc86cc2f0091f8dff7a0824ccf06b8
Placido Fernandez Declara <placido.fernandez@cern.ch>
2021-12-02 15:38:30 +0100

    add output MT writer

---
+++ k4MarlinWrapper/k4MarlinWrapper/LcioEventOutput.h
@@ -0,0 +1,59 @@
+/**
+ *   Copyright 2021 CERN
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ *   In applying this licence, CERN does not waive the privileges and immunities
+ *   granted to it by virtue of its status as an Intergovernmental Organization
+ *   or submit itself to any jurisdiction.
+ *
+ */
+
+#ifndef K4MARLINWRAPPER_LCIOEVENTOUTPUT_H
+#define K4MARLINWRAPPER_LCIOEVENTOUTPUT_H
+
+// ***
+// LCEventOutput: Write out LCIO events
+// ***
+
+
+#include <iostream>
+
+#include <GaudiAlg/GaudiAlgorithm.h>
+
+#include <EVENT/LCIO.h>
+#include <MT/LCWriter.h>
+#include <IMPL/LCEventImpl.h>
+#include <lcio.h>
+
+#include "k4MarlinWrapper/LCEventWrapper.h"
+
+
+class LcioEventOutput : public GaudiAlgorithm {
+public:
+  explicit LcioEventOutput(const std::string& name, ISvcLocator* pSL);
+  virtual ~LcioEventOutput() = default;
+  virtual StatusCode initialize() override final;
+  virtual StatusCode execute() override final;
+  virtual StatusCode finalize() override final;
+
+private:
+
+  MT::LCWriter* m_writer = nullptr;
+  
+  Gaudi::Property<std::string> m_write_mode{this, "WriteMode", ""};
+  Gaudi::Property<std::string> m_filename{this, "OutputFileName", ""};
+  
+};
+
+#endif
