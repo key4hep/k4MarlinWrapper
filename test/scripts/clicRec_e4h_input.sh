bb1438bd45b97ea847af477ce28855a7725d136f
Thomas Madlener <thomas.madlener@desy.de>
2022-12-07 17:07:20 +0100

    Add simple diff based tests to validate clicRec outputs

--- test/scripts/clicRec_e4h_input.sh
+++ test/scripts/clicRec_e4h_input.sh
@@ -20,8 +20,27 @@ k4run $TEST_DIR/gaudi_opts/clicRec_e4h_input.py
 input_num_events=$(python $TEST_DIR/python/root_num_events.py $TEST_DIR/inputFiles/ttbar1_edm4hep.root)
 output_num_events=$(python $TEST_DIR/python/root_num_events.py my_output.root)
 
-if [ "$input_num_events" == "$output_num_events" ]; then
-  echo "Input and output have same number of events"
-else
+# First check do we have the same number of events in input and output
+if [ "$input_num_events" != "$output_num_events" ]; then
   echo "ERROR: different number of events for input and output"
+  exit 1
+fi
+
+# Second check: contents (at least superficially)
+echo "Comparing contents of Output_REC.slcio"
+if ! diff <(anajob Output_REC.slcio) $TEST_DIR/inputFiles/anajob_Output_REC.expected; then
+  echo "File contents of REC slcio file are not as expected"
+  exit 1
+fi
+
+echo "Comparing contents of Output_DST.slcio"
+if ! diff <(anajob Output_DST.slcio) $TEST_DIR/inputFiles/anajob_Output_DST.expected; then
+  echo "File contents of DST slcio file are not as expected"
+  exit 1
+fi
+
+echo "Comparing contents of EDM4hep output file"
+if ! diff <(podio-dump -e 0:2 my_output.root) $TEST_DIR/inputFiles/podio-dump_my_output.expected; then
+  echo "File contents of EDM4hep file are not as expected"
+  exit 1
 fi
