e0194eccf2404dc8844efa9a085d1587417535b8
Thomas Madlener <thomas.madlener@desy.de>
2025-10-07 16:40:44 +0200

    Add test where EventHeader is not present
    To verify that without fix we get an error here

--- test/gaudi_opts/createEventHeader.py
+++ test/gaudi_opts/createEventHeader.py
@@ -25,6 +25,16 @@ from Configurables import (
     EventDataSvc,
 )
 from k4FWCore import ApplicationMgr, IOSvc
+from k4FWCore.parseArgs import parser
+
+parser.add_argument(
+    "--no-event-header",
+    help="Do not create EventHeaders",
+    default=False,
+    action="store_true",
+)
+
+args = parser.parse_known_args()[0]
 
 iosvc = IOSvc()
 
@@ -35,14 +45,19 @@ EDM4hep2Lcio.convertAll = False
 
 out = MarlinProcessorWrapper("out")
 out.ProcessorType = "LCIOOutputProcessor"
-out.Parameters = {"LCIOOutputFile": ["test.slcio"], "LCIOWriteMode": ["WRITE_NEW"]}
+out.Parameters = {
+    "LCIOOutputFile": ["test.slcio" if not args.no_event_header else "test_without_header.slcio"],
+    "LCIOWriteMode": ["WRITE_NEW"],
+}
 out.EDM4hep2LcioTool = EDM4hep2Lcio
 
+if args.no_event_header:
+    alg_list = [out]
+else:
+    alg_list = [eventHeaderCreator, out]
+
 ApplicationMgr(
-    TopAlg=[
-        eventHeaderCreator,
-        out,
-    ],
+    TopAlg=alg_list,
     EvtSel="NONE",
     EvtMax=2,
     ExtSvc=[EventDataSvc()],
